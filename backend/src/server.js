0a2535a538a5b152e8ae54391a8a953a2b7fe913
Haseeb Rajpoot <haseebrajpoots786@gmail.com>
2026-05-22 16:06:27 +0500

    start server method added

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,7 +28,11 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-app.listen(ENV.PORT, () => {
-  console.log("Server is up and running");
-  connectDB();
-});
+const startServer = async () => {
+  await connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log("Server is up and running");
+  });
+};
+
+startServer();
