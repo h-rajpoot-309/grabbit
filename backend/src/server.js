af482bf68698d77356104c963b11d83e7f46012d
Haseeb Rajpoot <haseebrajpoots786@gmail.com>
2026-06-04 00:19:27 +0500

    auth implement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import path from "path";
 import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { functions, inngest } from "./config/inngest.js";
 import { fileURLToPath } from "url";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
@@ -11,7 +13,10 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(express.json());
 app.use(clerkMiddleware());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
